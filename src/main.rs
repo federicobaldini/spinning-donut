c501fc71522e2fbd0e648f9cf913b4bcdb4f82d4
Federico Baldini <federico.baldini7@gmail.com>
2022-10-08 12:46:57 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use std::io;
+
+fn main() {
+  let (mut A, mut B): (f32, f32) = (0.0, 0.0);
+  let z: [f32; 1760];
+  let b: [char; 1760];
+
+  print!("\n1b2J");
+  loop {
+    for j in (0..626).step_by(1) {
+      for i in (0..626).step_by(8) {
+
+      }
+    }
+  }
+}
